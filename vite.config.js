9836821cbfab3f40356139541017fab1296865e3
Kotha Srivibhu <kothasrivibhu.23bds1057@gmail.com>
2025-05-31 12:16:37 +0530

    Update paths for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Bsjrdegreecollege/',
   plugins: [react()],
+  base: '/bsjr/',
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.jsx?$/,
