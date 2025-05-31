4afb8fcbd653fd68949c71cbee9c9bfbdc272b50
Kotha Srivibhu <kothasrivibhu.23bds1057@gmail.com>
2025-05-31 12:26:36 +0530

    Fix script path and build configuration

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,12 @@ export default defineConfig({
   plugins: [react()],
   base: '/bsjr/',
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name].[ext]'
       }
     }
